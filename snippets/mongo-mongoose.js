fdd60550d21178f3cce971d38551427c25e509ed
Prashant Kumar Verma <prashantvrm@gmail.com>
2021-11-14 19:55:00 -0800

    Update mongo-mongoose.js

--- snippets/mongo-mongoose.js
+++ snippets/mongo-mongoose.js
@@ -1,3 +1,11 @@
+const User = mongoose.model('User', {
+    mobile_number: Number,
+    firstName: String,
+    lastName: String,
+    vehicle: Array,
+    email: String
+});
+
 // Find data using mobile_number then remove item from array[vehicle] and update firstName
 const findOneAndUpdate = User.findOneAndUpdate({ mobile_number: '8562981231' }, { $pull: { vehicle: 'up33kk' }, firstName: 'Selena' })
 findOneAndUpdate.then((err, data) => {
